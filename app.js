7b66b28f0aa759709007aa5e4dbb04c1f1b3f8bf
ahmetcoskuncode <107874517+ahmetcoskuncode@users.noreply.github.com>
2022-09-29 14:41:34 +0300

    Sohbet Uygulama

---
+++ app.js
@@ -0,0 +1,34 @@
+﻿
+ const { info } = require("console");
+var express = require("express");
+ var app = express();
+ var http = require("http").createServer(app);
+ var io = require("socket.io")(http);
+ var users = [];
+ var messages = [];
+ app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+ });
+
+ io.on("connection",(socket)=>{
+    socket.on("newUser",(userName)=>{
+        users.push({
+            id:socket.id,
+            name:userName
+        });
+        io.emit("users",users);
+    });
+    socket.on("sendMessageClient",(info)=>{
+        messages.push(info);
+        io.emit("messagesServer",messages);
+        
+    });
+ });
+
+ http.listen(3000,()=>{console.log("Server Çalışıyor")});
+
+
+
+
+
+
